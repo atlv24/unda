783e4aa342d8303d023b6dd2c082ece52333d079
Eben Kadile <eben.kadiler24@gmail.com>
2024-09-15 01:07:18 +0200

    added documentation as well as dynamic batch size stuff

--- src/training/optimizers.rs
+++ src/training/optimizers.rs
@@ -1,27 +1,69 @@
+use half::vec;
+use smallvec::SmallVec;
 use xla::{ElementType, Literal};
 
 use crate::graph::context::Result;
-use crate::graph::{Context, NodeIdentifier};
+use crate::graph::{Context, NodeIdentifier, Shape};
 use crate::graph::{ContextError, Node};
 
 pub trait Optimizer<U> {
-    // The Context should take parameters, gradients, and state in that order
-    // should return new parameters, new state in that order
+    /// This trait represents an optimizer which takes network parameters,
+    /// gradients of those parameters, optional "target" inputs,
+    /// and its own state to return new parameters and update its own state.
+
+    /// Returns a context which takes parameters, gradients, optional targets inputs,
+    /// and state in that order to return new parameters and new state.
     fn get_step(&self) -> &Context;
+
+    /// Count parameter tensors which the optimizer acts on.
     fn n_params(&self) -> usize;
+
+    /// How many of the targets from the `SupervisedTrainer` will the optimizer take?
+    /// Most optimizers will probably not take any targets from the trainer,
+    /// but batch normalization with a dynamic batch size will.
+    fn n_target_inputs(&self) -> usize;
+
+    /// Number of tensors in the optimizer's state
     fn state_size(&self) -> usize;
+
+    /// Point to the old parameters in the step context.
     fn get_old_params(&self) -> Vec<NodeIdentifier>;
+
+    /// Point to the gradients in the step context.
     fn get_gradients(&self) -> Vec<NodeIdentifier>;
+
+    /// Point to the target inputs in the step context.
+    fn get_target_inputs(&self) -> Vec<NodeIdentifier>;
+
+    /// Point to the new parameters in the step context.
     fn get_new_params(&self) -> Vec<NodeIdentifier>;
+
+    /// Point to the old state in the step context.
     fn get_old_state(&self) -> Vec<NodeIdentifier>;
+
+    /// Point to the new state in the step context.
     fn get_new_state(&self) -> Vec<NodeIdentifier>;
+
+    /// Get any user-specified parameters to the optimizer (such as learning rate or momentum).
     fn get_user_params(&self) -> U;
-    fn new(user_params: U, model_params: Vec<NodeIdentifier>, model: &Context) -> Self;
+
+    /// Create the optimizer from some network parameters and a network context.
+    fn new(user_params: U, network_params: Vec<NodeIdentifier>, network: &Context) -> Self;
+
+    /// Initialize the state of the optimizer.
     fn init_state(&self) -> Vec<Literal>;
 }
 
 #[derive(Clone)]
 pub enum LearningRateSchedule {
+    /// This enum specifies the options for learning rate schedules.
+    ///
+    /// `Constant` will simply give a constant learning rate.
+    /// `ExpDecay` starts from `init_lr` and multiplies it be `coefficient`
+    /// every `decay_every` optimization steps.
+    /// `CosineAnneal` states from `init_lr` and increases to `max_lr`
+    /// after `n_steps_to_peak` by following a cosine curve.
+    /// `Then` executes `schedule_1` for `n_steps` and then executes `schedule_2`.
     Constant(f32),
     ExpDecay {
         init_lr: f32,
@@ -41,6 +83,7 @@ pub enum LearningRateSchedule {
 }
 
 enum LRSchedOffset {
+    /// Helper enum for converting user-facing `LearningRateSchedule` to a Context.
     Constant(f32, usize),
     ExpDecay {
         init_lr: f32,
@@ -92,7 +135,6 @@ impl LearningRateSchedule {
                     n_steps,
                     schedule_2,
                 } => LRSchedOffset::Then {
-                    // why clone here???
                     schedule_1: Box::new(zero_offset(schedule_1.as_ref())),
                     n_steps: *n_steps,
                     schedule_2: Box::new(zero_offset(schedule_2.as_ref())),
@@ -145,10 +187,18 @@ impl LearningRateSchedule {
         }
         recurse(&zero_offset(self), 0)
     }
+
+    /// From the specific learning rate schedule compute
+    /// (node identifier for the step/input,
+    /// context for computing the learning rate from the step,
+    /// node identifier for the learning rate/output)
+    pub fn build_context(&mut self) -> Result<(NodeIdentifier, Context, NodeIdentifier)> {
+        self.offset().build_context()
+    }
 }
 
 impl LRSchedOffset {
-    pub fn build_context(&self) -> Result<(NodeIdentifier, Context, NodeIdentifier)> {
+    fn build_context(&self) -> Result<(NodeIdentifier, Context, NodeIdentifier)> {
         let mut scheduler = Context::new();
         let iteration = scheduler.parameter("iteration", [], ElementType::U32)?;
 
@@ -213,6 +263,8 @@ impl LRSchedOffset {
 }
 
 pub struct ChainedOptimizer<U> {
+    /// This struct represents two different optimizers operating on
+    /// two different subsets of network parameters.
     step: Context,
     n_params: usize,
     state_size: usize,
@@ -224,6 +276,11 @@ pub struct ChainedOptimizer<U> {
     pub user_params: U,
 }
 
+/// Chain the optimizers `opt1` and `opt2` together.
+///
+/// The resulting optimizers will expect all the parameters
+/// of `opt1` following by all the paraemters of `opt2`.
+/// Likewise for the gradients, and state.
 pub fn chain<U1, U2, O1: Optimizer<U1>, O2: Optimizer<U2>>(
     opt1: O1,
     opt2: O2,
@@ -254,8 +311,8 @@ pub fn chain<U1, U2, O1: Optimizer<U1>, O2: Optimizer<U2>>(
     })
 }
 
-//*/
 pub struct SGD {
+    /// This struct represents stochastic gradient descent on a particular set of parameters.
     step: Context,
     n_params: usize,
     old_params: Vec<NodeIdentifier>,
@@ -273,6 +330,9 @@ impl Optimizer<LearningRateSchedule> for SGD {
     fn n_params(&self) -> usize {
         self.n_params
     }
+    fn n_target_inputs(&self) -> usize {
+        0
+    }
     fn state_size(&self) -> usize {
         1
     }
@@ -281,6 +341,9 @@ impl Optimizer<LearningRateSchedule> for SGD {
     }
     fn get_gradients(&self) -> Vec<NodeIdentifier> {
         self.grads.clone()
+    }
+    fn get_target_inputs(&self) -> Vec<NodeIdentifier> {
+        Vec::new()
     }
     fn get_new_params(&self) -> Vec<NodeIdentifier> {
         self.new_params.clone()
@@ -294,33 +357,34 @@ impl Optimizer<LearningRateSchedule> for SGD {
     fn get_user_params(&self) -> LearningRateSchedule {
         self.lr_schedule.clone()
     }
-    // TODO WILL FAIL NEED PROPER GRAPH MERGING
+
     fn new(
         lr_schedule: LearningRateSchedule,
-        model_params: Vec<NodeIdentifier>,
-        model: &Context,
+        network_params: Vec<NodeIdentifier>,
+        network: &Context,
     ) -> SGD {
         let build = || {
             let (iter_node, scheduler, lr_node) = lr_schedule.offset().build_context()?;
 
             let mut step = Context::new();
 
-            let n_params = model_params.len();
+            let n_params = network_params.len();
 
-            let dtype = model.nodes[model_params[0]].dtype;
+            let dtype = network.nodes[network_params[0]].dtype;
 
             let mut old_params = Vec::new();
             let mut grads = Vec::new();
             let mut new_params = Vec::new();
 
-            for (i, node_id) in model_params.iter().enumerate() {
-                let model_param = &model.nodes[*node_id];
-                let old_param = step.parameter("", model_param.shape.clone(), model_param.dtype)?;
+            for (i, node_id) in network_params.iter().enumerate() {
+                let network_param = &network.nodes[*node_id];
+                let old_param =
+                    step.parameter("", network_param.shape.clone(), network_param.dtype)?;
                 old_params.push(old_param);
             }
-            for (i, node_id) in model_params.iter().enumerate() {
-                let model_param = &model.nodes[*node_id];
-                let grad = step.parameter("", model_param.shape.clone(), model_param.dtype)?;
+            for (i, node_id) in network_params.iter().enumerate() {
+                let network_param = &network.nodes[*node_id];
+                let grad = step.parameter("", network_param.shape.clone(), network_param.dtype)?;
                 grads.push(grad);
             }
 
@@ -358,6 +422,17 @@ impl Optimizer<LearningRateSchedule> for SGD {
 }
 
 pub struct BatchNormOptimizer {
+    /// This struct represents a batch normalization optimizer on
+    /// the mean and variance of a batchnorm layer.
+    ///
+    /// This is an atypical concept for a machine learning library.
+    /// How does one mesh the typical gradient descent of all parameters of a network
+    /// with the online mean/variance estimation of batchnorm?
+    /// This is our solution: `BatchNorm` is its own operation which
+    /// interacts with autodiff in an unusual way. Specifically, the mean and variance
+    /// parameters of the batchnorm layer simply have the inputs to the layer
+    /// as their gradients.
+    /// This optimizer takes those inputs (as gradients) and computes the rolling mean/variance.
     step: Context,
     old_params: Vec<NodeIdentifier>,
     grads: Vec<NodeIdentifier>,
@@ -373,6 +448,9 @@ impl Optimizer<f32> for BatchNormOptimizer {
     fn n_params(&self) -> usize {
         2
     }
+    fn n_target_inputs(&self) -> usize {
+        0
+    }
     fn state_size(&self) -> usize {
         0
     }
@@ -381,6 +459,9 @@ impl Optimizer<f32> for BatchNormOptimizer {
     }
     fn get_gradients(&self) -> Vec<NodeIdentifier> {
         self.grads.clone()
+    }
+    fn get_target_inputs(&self) -> Vec<NodeIdentifier> {
+        Vec::new()
     }
     fn get_new_params(&self) -> Vec<NodeIdentifier> {
         self.new_params.clone()
@@ -397,7 +478,7 @@ impl Optimizer<f32> for BatchNormOptimizer {
     fn new(
         momentum: f32,
         batchnorm_params: Vec<NodeIdentifier>,
-        model: &Context,
+        network: &Context,
     ) -> BatchNormOptimizer {
         let build = || {
             let mut step = Context::new();
@@ -408,25 +489,29 @@ impl Optimizer<f32> for BatchNormOptimizer {
                 batchnorm_params[2],
             );
 
-            let dtype = model.nodes[mu].dtype;
+            let dtype = network.nodes[mu].dtype;
             let mom = step.scalar(momentum, dtype)?;
             let mut mom1 = step.scalar(1.0, dtype)?;
             mom1 = step.sub(mom1, mom)?;
 
             let old_mu =
-                step.parameter("", model.nodes[mu].shape.clone(), model.nodes[mu].dtype)?;
+                step.parameter("", network.nodes[mu].shape.clone(), network.nodes[mu].dtype)?;
             // grad_mu and grad_sigma are actually the same as x here!
             // they are treated as gradients for the sake of API harmonization
-            let old_sigma =
-                step.parameter("", model.nodes[mu].shape.clone(), model.nodes[mu].dtype)?;
-            let grad_mu = step.parameter("", model.nodes[x].shape.clone(), model.nodes[x].dtype)?;
+            let old_sigma = step.parameter(
+                "",
+                network.nodes[sigma].shape.clone(),
+                network.nodes[sigma].dtype,
+            )?;
+            let grad_mu =
+                step.parameter("", network.nodes[x].shape.clone(), network.nodes[x].dtype)?;
             let grad_sigma =
-                step.parameter("", model.nodes[x].shape.clone(), model.nodes[x].dtype)?;
+                step.parameter("", network.nodes[x].shape.clone(), network.nodes[x].dtype)?;
             let old_params = vec![old_mu, old_sigma];
             let grads = vec![grad_mu, grad_sigma];
 
             let mut mean = step.reduce_mean(grad_mu, 0, true)?;
-            for i in 1..model.nodes[mu].shape.ndims() - 1 {
+            for i in 1..network.nodes[mu].shape.ndims() - 1 {
                 mean = step.reduce_mean(mean, i as i64, true)?;
             }
             let mul = step.mul(mom, old_mu)?;
@@ -436,7 +521,7 @@ impl Optimizer<f32> for BatchNormOptimizer {
             let zero_centered = step.sub(grad_mu, mean)?;
             let squared = step.mul(zero_centered, zero_centered)?;
             let mut std = step.reduce_mean(squared, 0, true)?;
-            for i in 1..model.nodes[mu].shape.ndims() - 1 {
+            for i in 1..network.nodes[mu].shape.ndims() - 1 {
                 std = step.reduce_mean(mean, i as i64, true)?;
             }
             let mul = step.mul(mom, old_sigma)?;
@@ -449,6 +534,147 @@ impl Optimizer<f32> for BatchNormOptimizer {
                 step,
                 old_params,
                 grads,
+                new_params,
+                dummy_state: Vec::new(),
+                momentum,
+            })
+        };
+
+        build().expect("Failed to new Batch Normalization optimizer")
+    }
+    fn init_state(&self) -> Vec<Literal> {
+        return Vec::new();
+    }
+}
+
+pub struct DynamicBatchNormOptimizer {
+    /// Same idea as above except with a dynamic batch size.
+    /// The idea here is simply that a batch mask of floating point 0s
+    /// and 1s specifying which passes actually have inputs will
+    /// be passed to the SupervisedTrainer. The mean/variance
+    /// will be computed accounting for this batch mask,
+    step: Context,
+    old_params: Vec<NodeIdentifier>,
+    grads: Vec<NodeIdentifier>,
+    batch_mask: NodeIdentifier,
+    new_params: Vec<NodeIdentifier>,
+    dummy_state: Vec<NodeIdentifier>,
+    pub momentum: f32,
+}
+
+impl Optimizer<f32> for DynamicBatchNormOptimizer {
+    fn get_step(&self) -> &Context {
+        &self.step
+    }
+    fn n_params(&self) -> usize {
+        2
+    }
+    fn n_target_inputs(&self) -> usize {
+        1
+    }
+    fn state_size(&self) -> usize {
+        0
+    }
+    fn get_old_params(&self) -> Vec<NodeIdentifier> {
+        self.old_params.clone()
+    }
+    fn get_gradients(&self) -> Vec<NodeIdentifier> {
+        self.grads.clone()
+    }
+    fn get_target_inputs(&self) -> Vec<NodeIdentifier> {
+        vec![self.batch_mask]
+    }
+    fn get_new_params(&self) -> Vec<NodeIdentifier> {
+        self.new_params.clone()
+    }
+    fn get_old_state(&self) -> Vec<NodeIdentifier> {
+        self.dummy_state.clone()
+    }
+    fn get_new_state(&self) -> Vec<NodeIdentifier> {
+        self.dummy_state.clone()
+    }
+    fn get_user_params(&self) -> f32 {
+        self.momentum
+    }
+    fn new(
+        momentum: f32,
+        batchnorm_params: Vec<NodeIdentifier>,
+        network: &Context,
+    ) -> DynamicBatchNormOptimizer {
+        let build = || {
+            let mut step = Context::new();
+
+            let (mu, sigma, x) = (
+                batchnorm_params[0],
+                batchnorm_params[1],
+                batchnorm_params[2],
+            );
+
+            let dtype = network.nodes[mu].dtype;
+            let mom = step.scalar(momentum, dtype)?;
+            let mut mom1 = step.scalar(1.0, dtype)?;
+            mom1 = step.sub(mom1, mom)?;
+
+            let old_mu =
+                step.parameter("", network.nodes[mu].shape.clone(), network.nodes[mu].dtype)?;
+            // grad_mu and grad_sigma are actually the same as x here!
+            // they are treated as gradients for the sake of API harmonization
+            let old_sigma = step.parameter(
+                "",
+                network.nodes[sigma].shape.clone(),
+                network.nodes[sigma].dtype,
+            )?;
+            let grad_mu =
+                step.parameter("", network.nodes[x].shape.clone(), network.nodes[x].dtype)?;
+            let grad_sigma =
+                step.parameter("", network.nodes[x].shape.clone(), network.nodes[x].dtype)?;
+            let old_params = vec![old_mu, old_sigma];
+            let grads = vec![grad_mu, grad_sigma];
+
+            let batch_size = network.nodes[x].shape.sizes[0];
+            let batch_mask = step.parameter(
+                "",
+                [batch_size],
+                network.nodes[x].dtype,
+            )?;
+            let mut new_shape = SmallVec::new();
+            new_shape.push(batch_size);
+            for i in 1..network.nodes[x].shape.ndims() {
+                new_shape.push(1);
+            }
+            let mask_reshaped = step.reshape(batch_mask, Shape{ sizes: new_shape })?;
+            let denominator = step.reduce_sum(batch_mask, 0, false)?;
+
+            let grad_mu_masked = step.mul(mask_reshaped, grad_mu)?;
+
+            let mut mean = step.reduce_sum(grad_mu_masked, 0, true)?;
+            mean = step.div(mean, denominator)?;
+            for i in 1..network.nodes[mu].shape.ndims() - 1 {
+                mean = step.reduce_mean(mean, i as i64, true)?;
+            }
+            let mul = step.mul(mom, old_mu)?;
+            let mul1 = step.mul(mom1, mean)?;
+            let new_mu = step.add(mul, mul1)?;
+
+            let zero_centered = step.sub(grad_mu, mean)?;
+            let zero_centered_masked = step.mul(mask_reshaped, zero_centered)?;
+            let squared = step.mul(zero_centered_masked, zero_centered_masked)?;
+            let mut std = step.reduce_sum(squared, 0, true)?;
+            std = step.div(std, denominator)?;
+            for i in 1..network.nodes[mu].shape.ndims() - 1 {
+                std = step.reduce_mean(mean, i as i64, true)?;
+            }
+            let mul = step.mul(mom, old_sigma)?;
+            let mul1 = step.mul(mom1, std)?;
+            let new_sigma = step.add(mul, mul1)?;
+
+            let new_params = vec![new_mu, new_sigma];
+
+            Ok::<DynamicBatchNormOptimizer, ContextError>(DynamicBatchNormOptimizer {
+                step,
+                old_params,
+                grads,
+                batch_mask,
                 new_params,
                 dummy_state: Vec::new(),
                 momentum,
